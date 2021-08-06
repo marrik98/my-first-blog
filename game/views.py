6df70f89bded6cec5450fc3c90c7c3408f8887a8
Mark <mark141198@ya.ru>
2021-08-06 13:11:30 +0300

    Modified templates to display games from database.

--- game/views.py
+++ game/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from .models import Game
+from django.utils import timezone
 
 # Create your views here.
 
 def game_list(request):
-    return render(request, 'game/game_list.html', {})
+    games = Game.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'game/game_list.html', {'games': games})
